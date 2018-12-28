68ec3eeecd8d9ed8b31f99f38dbb57afc566ef96
AMZDLHDT2\AMZDLHDT2 <dhananjay.verma@armezo.com>
2018-12-28 18:39:51 +0530

    Spring Elastic Search Project Structure

---
+++ src/main/java/com/getknowledge/elastic/springelasticsearch/SpringElasticSearchApplication.java
@@ -0,0 +1,14 @@
+package com.getknowledge.elastic.springelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringElasticSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringElasticSearchApplication.class, args);
+	}
+
+}
+
