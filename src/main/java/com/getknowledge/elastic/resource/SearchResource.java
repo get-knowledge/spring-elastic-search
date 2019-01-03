2a6cae28003c0813e1289ebfd8864402f62c9186
AMZDLHDT2\AMZDLHDT2 <dhananjay.verma@armezo.com>
2019-01-03 18:00:43 +0530

    Add Jar Dependencies in pom.xml file spring-boot-starter-data-elasticsearch

--- src/main/java/com/getknowledge/elastic/resource/SearchResource.java
+++ src/main/java/com/getknowledge/elastic/resource/SearchResource.java
@@ -1,10 +1,28 @@
 package com.getknowledge.elastic.resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.getknowledge.elastic.model.Users;
+import com.getknowledge.elastic.repository.UsersRepository;
 
 @RestController
 @RequestMapping("/rest/search")
 public class SearchResource {
+	
+	
+	@Autowired
+	UsersRepository usersRepository;
+	
+	@GetMapping(value="")
+	public List<Users> searchName(@PathVariable final String text){
+		
+		return usersRepository.findByName(text);
+	}
 
 }
